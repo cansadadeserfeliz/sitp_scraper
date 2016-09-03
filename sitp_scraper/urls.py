35cb8b9b620349aaf64d2f5132a770571fd02fd2
Vera Mazhuga <vero4ka.ru@gmail.com>
2016-09-02 21:38:23 -0500

    Home page

--- sitp_scraper/urls.py
+++ sitp_scraper/urls.py
@@ -1,4 +1,4 @@
-"""sitp_scrapper URL Configuration
+"""sitp_scraper URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- sitp_scraper/wsgi.py
+++ sitp_scraper/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sitp_scrapper project.
+WSGI config for sitp_scraper project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sitp_scrapper.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sitp_scraper.settings")
 
 application = get_wsgi_application()
