ba6fdcf980487f01959565ca8382699d7de212bf
Vera Mazhuga <vero4ka.ru@gmail.com>
2017-01-07 23:18:03 -0500

    Add Emoji

---
+++ sitp_bot/utils.py
@@ -0,0 +1,8 @@
+EMOJI_CODES = {
+    'alarm_clock': '\u23f0',
+    'clock_face_ten_o_clock': '\U0001F559',
+    'oncoming_bus': '\U0001F68D',
+    'disappointed': '\U0001f61e',
+    'smiling_face_with_smiling_eyes': '\U0001F60A',
+    'rain': '\U00002614',
+}
