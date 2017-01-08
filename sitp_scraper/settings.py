93e6d95974e546e482cdb8d993430b2bd822e644
Vera Mazhuga <vero4ka.ru@gmail.com>
2017-01-07 23:50:08 -0500

    Change release for sentry

--- sitp_scraper/settings.py
+++ sitp_scraper/settings.py
@@ -170,7 +170,7 @@ RAVEN_CONFIG = {
     'dsn': os.getenv('SENTRY_URL', ''),
     # If you are using git, you can also automatically configure the
     # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+    'release': '1.0',
 }
 
 try:
