e312a64299a80edb3407cef6f72e5365114eacee
Vera Mazhuga <vero4ka.ru@gmail.com>
2017-02-04 10:47:15 -0500

    FB bot verification

--- sitp_bot/urls.py
+++ sitp_bot/urls.py
@@ -2,12 +2,19 @@ from django.conf.urls import url
 
 from django.views.generic import TemplateView
 
-from .views import CommandReceiveView
+from .views import CommandReceiveView, FacebookCommandReceiveView
 
 urlpatterns = [
     url(
         r'(?P<bot_token>.+)/$',
         CommandReceiveView.as_view(),
         name='index',
+    ),
+
+
+    url(
+        r'fb-webhook/(?P<bot_keyword>.+)/$',
+        FacebookCommandReceiveView.as_view(),
+        name='fb_webhook',
     ),
 ]
