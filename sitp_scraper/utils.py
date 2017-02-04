e1d271c781fe19fcc6810882a5227dd6511383aa
Vera Mazhuga <vero4ka.ru@gmail.com>
2017-02-04 18:46:00 -0500

    Return route info

--- sitp_scraper/utils.py
+++ sitp_scraper/utils.py
@@ -2,7 +2,7 @@ from django.contrib.gis.geos import Point
 from django.contrib.gis.db.models.functions import Distance
 from django.contrib.gis.measure import D
 
-from .models import BusStation
+from .models import BusStation, Route
 
 
 def get_closest_station(latitude, longitude):
@@ -14,3 +14,10 @@ def get_closest_station(latitude, longitude):
     ).annotate(
         distance=Distance('location', ref_location)
     ).order_by('distance').first()
+
+
+def get_route(text):
+    text = text.lower().strip()
+    text = ''.join(filter(str.isalpha, text))
+    text = text.replace('bus', '').replace(' ', '')
+    return Route.objects.filter(code__iexact=text).first()
